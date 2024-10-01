7b879926fcf0f662a28ac814a31feed2b3741eea
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2024-10-02 00:47:24 +0700

    Adding functionality to the cards on the [index.js]: createCard(), deleteCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
+const addCardButton = document.querySelector('.profile__add-button');
+const placeList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+// The function of uploading a card to a page
 
-// @todo: Функция создания карточки
+function createCard(nameCard, imgSrc) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item.card').cloneNode(true);
 
-// @todo: Функция удаления карточки
+    cardElement.querySelector('.card__image').src = imgSrc;
+    cardElement.querySelector('.card__image').alt = nameCard;
+    
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => { 
+        deleteCard(cardElement)
+    });
 
-// @todo: Вывести карточки на страницу
+    cardElement.querySelector('.card__title').textContent = nameCard;
+
+    return placeList.append(cardElement);
+}
+
+// Card deletion function
+
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+// Event handler for clicking the add card button
+
+addCardButton.addEventListener('click', () => {
+    initialCards.forEach(item => { 
+        createCard(item.name, item.link);
+    });
+});
